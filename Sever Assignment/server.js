16c79869196a11fcfb4ccecd7fcb7133acef2779
Paul Udor <paul.udor@gmail.com>
2024-01-13 12:36:41 -0500

    First nodejs Assignment

---
+++ Sever Assignment/server.js
@@ -0,0 +1,21 @@
+// Calling the 'http' module
+const http = require('http');
+
+// Setting the hostname and port number
+const hostname = 'localhost';
+const port = 8900;
+
+// Establishing the HTTP server
+const server = http.createServer((req, res) => {
+  // Setting the response headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('My name is Paul Udor');
+
+  console.log(`Request received: ${req.method} ${req.url}`);
+});
+
+// Listening in for incoming requests on the specified hostname and port
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
