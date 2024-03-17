9980bb91ac0784c3c27de8aa5086bcc8b7e09554
Paul Udor <paul.udor@gmail.com>
2024-03-17 19:14:40 +0000

    new port

--- NodeJs Assignment/simple-server-http-modules.js
+++ NodeJs Assignment/simple-server-http-modules.js
@@ -76,7 +76,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
